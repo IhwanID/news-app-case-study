5db78ba5f4f0d40ebbb5990d0080d451aee7189d
Ihwan <Ihwan@macbookpro15.local>
2022-03-06 15:56:13 +0700

    init project

---
+++ TopNews/TopNews/TopNews.h
@@ -0,0 +1,18 @@
+//
+//  TopNews.h
+//  TopNews
+//
+//  Created by Ihwan on 06/03/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TopNews.
+FOUNDATION_EXPORT double TopNewsVersionNumber;
+
+//! Project version string for TopNews.
+FOUNDATION_EXPORT const unsigned char TopNewsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TopNews/PublicHeader.h>
+
+
