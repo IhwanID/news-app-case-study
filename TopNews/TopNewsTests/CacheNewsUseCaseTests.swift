b30f541eac64a55c7f2d53796c962e721f5414b3
ihwan <ihwan.id@icloud.com>
2022-03-28 20:44:10 +0700

    Save command succeeds on successful cache insertion

--- TopNews/TopNewsTests/CacheNewsUseCaseTests.swift
+++ TopNews/TopNewsTests/CacheNewsUseCaseTests.swift
@@ -57,13 +57,17 @@ class NewsStore {
         deletionCompletions[index](nil)
     }
     
-    func insert(_ items: [NewsItem], timestamp: Date ,completion: @escaping InsertionCompletion) {
+    func insert(_ items: [NewsItem], timestamp: Date, completion: @escaping InsertionCompletion) {
         insertionCompletions.append(completion)
         receivedMessages.append(.insert(items, timestamp))
     }
     
     func completeInsertion(with error: Error, at index: Int = 0) {
         insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0){
+        insertionCompletions[index](nil)
     }
 }
 
@@ -142,6 +146,24 @@ class CacheNewsUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let items = [uniqueItem(), uniqueItem()]
+        let (sut, store) = makeSUT()
+        let exp = expectation(description: "Wait for save completion")
+        
+        var receivedError: Error?
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError)
     }
     
     // MARK: - Helpers
