cec5a00cd373770a946caea110eb650dd99f39bf
ihwan <ihwan.id@icloud.com>
2022-03-30 09:58:07 +0700

    move LocalNewsItem to its own file

---
+++ TopNews/TopNews/NewsCache/LocalNewsItem.swift
@@ -0,0 +1,30 @@
+//
+//  LocalNewsItem.swift
+//  TopNews
+//
+//  Created by Ihwan on 30/03/22.
+//
+
+import Foundation
+
+public struct LocalNewsItem: Equatable {
+    public let title: String
+    public let author: String?
+    public let source: String
+    public let description: String
+    public let content: String?
+    public let newsURL: URL
+    public let imageURL: URL?
+    public let publishedAt: Date
+    
+    public init(title: String, author: String?, source: String, description: String, content: String?, newsURL: URL, imageURL: URL?, publishedAt: Date) {
+        self.title = title
+        self.author = author
+        self.source = source
+        self.description = description
+        self.content = content
+        self.newsURL = newsURL
+        self.imageURL = imageURL
+        self.publishedAt = publishedAt
+    }
+}
