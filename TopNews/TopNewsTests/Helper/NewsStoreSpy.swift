12f3d0986055f09b76c08cc21d2a9bd8d2213549
Ihwan <ihwan.id@icloud.com>
2022-04-02 17:00:13 +0700

    Extract NewsStoreSpy into a shared scope to remove duplication

---
+++ TopNews/TopNewsTests/Helper/NewsStoreSpy.swift
@@ -0,0 +1,47 @@
+//
+//  NewsStoreSpy.swift
+//  TopNewsTests
+//
+//  Created by Ihwan on 02/04/22.
+//
+
+import Foundation
+@testable import TopNews
+
+class NewsStoreSpy: NewsStore {
+    enum ReceivedMessage: Equatable {
+        case deleteCachedNews
+        case insert([LocalNewsItem], Date)
+    }
+
+    private(set) var receivedMessages = [ReceivedMessage]()
+
+    private var deletionCompletions = [DeletionCompletion]()
+    private var insertionCompletions = [InsertionCompletion]()
+
+    func deleteCachedNews(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCachedNews)
+    }
+
+    func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+
+    func completeDeletionSuccessfully(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+
+    func insert(_ news: [LocalNewsItem], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(news, timestamp))
+    }
+
+    func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
