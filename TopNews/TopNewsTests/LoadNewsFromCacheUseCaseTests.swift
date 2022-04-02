6009bea2974c6451f136109a3f613da5b5de8aec
Ihwan <ihwan.id@icloud.com>
2022-04-03 04:55:39 +0700

    Load command does not delete less than seven days old cache

--- TopNews/TopNewsTests/LoadNewsFromCacheUseCaseTests.swift
+++ TopNews/TopNewsTests/LoadNewsFromCacheUseCaseTests.swift
@@ -92,6 +92,18 @@ class LoadNewsFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_load_doesNotDeleteCacheOnLessThanSevenDaysOldCache() {
+        let news = uniqueNews()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.load { _ in }
+        store.completeRetrieval(with: news.local, timestamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalNewsLoader, store: NewsStoreSpy) {
