978d27b5638a723120562d13d8fa8cbd3924c341
Ihwan <ihwan.id@icloud.com>
2022-04-02 17:23:18 +0700

    Load command delivers no news on empty cache

--- TopNews/TopNewsTests/LoadNewsFromCacheUseCaseTests.swift
+++ TopNews/TopNewsTests/LoadNewsFromCacheUseCaseTests.swift
@@ -46,6 +46,26 @@ class LoadNewsFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
     
+    func test_load_deliversNoNresOnEmptyCache() {
+            let (sut, store) = makeSUT()
+            let exp = expectation(description: "Wait for load completion")
+
+            var receivedNews: [NewsItem]?
+            sut.load { result in
+                switch result {
+                case let .success(items):
+                    receivedNews = items
+                default:
+                    XCTFail("Expected success, got \(result) instead")
+                }
+                exp.fulfill()
+            }
+
+            store.completeRetrievalWithEmptyCache()
+            wait(for: [exp], timeout: 1.0)
+
+            XCTAssertEqual(receivedNews, [])
+        }
     
     // MARK: - Helpers
     
