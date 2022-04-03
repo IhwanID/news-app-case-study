366ea447814c06563292134b6451e6085bc24cea
Ihwan <ihwan.id@icloud.com>
2022-04-03 22:38:58 +0700

    Deleting non-empty cache empties previously inserted cache

--- TopNews/TopNewsTests/NewsCache/CodableNewsStoreTests.swift
+++ TopNews/TopNewsTests/NewsCache/CodableNewsStoreTests.swift
@@ -79,6 +79,11 @@ class CodableNewsStore {
     }
     
     func deleteCachedNews(completion: @escaping NewsStore.DeletionCompletion) {
+        guard FileManager.default.fileExists(atPath: storeURL.path) else {
+            return completion(nil)
+        }
+        
+        try! FileManager.default.removeItem(at: storeURL)
         completion(nil)
     }
 }
@@ -173,6 +178,20 @@ class CodableNewsStoreTests: XCTestCase {
         
         sut.deleteCachedNews { deletionError in
             XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        
+        expect(sut, toRetrieve: .empty)
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        let sut = makeSUT()
+        insert((uniqueNews().local, Date()), to: sut)
+        
+        let exp = expectation(description: "Wait for cache deletion")
+        sut.deleteCachedNews { deletionError in
+            XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
             exp.fulfill()
         }
         wait(for: [exp], timeout: 1.0)
