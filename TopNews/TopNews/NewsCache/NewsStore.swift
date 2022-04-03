44cc0429bf6d06c2430c438413580f194ecca086
Ihwan <ihwan.id@icloud.com>
2022-04-04 05:26:45 +0700

    add comments to document that completion handler in NewsStore and HTTPClient implementation can be invoked in any thread. Clients are responsible to dispatch to appropriate threads, if needed

--- TopNews/TopNews/NewsCache/NewsStore.swift
+++ TopNews/TopNews/NewsCache/NewsStore.swift
@@ -18,7 +18,15 @@ protocol NewsStore {
     typealias InsertionCompletion = (LocalNewsLoader.SaveResult) -> Void
     typealias RetrievalCompletion = (RetrieveCachedNewsResult) -> Void
     
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func deleteCachedNews(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func insert(_ items: [LocalNewsItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to dispatch to appropriate threads, if needed.
     func retrieve(completion: @escaping RetrievalCompletion)
 }
