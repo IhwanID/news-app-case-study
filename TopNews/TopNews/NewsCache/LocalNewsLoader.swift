0e3b506a5a6adabfdc6e2f7c424b92deda6ee914
Ihwan <ihwan.id@icloud.com>
2022-04-03 17:26:57 +0700

    make NewsCachePolicy static since it doesn't keep any state

--- TopNews/TopNews/NewsCache/LocalNewsLoader.swift
+++ TopNews/TopNews/NewsCache/LocalNewsLoader.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 private class NewsCachePolicy {
-    private let calendar = Calendar(identifier: .gregorian)
+    private init() {}
     
-    private var maxCacheAgeInDays: Int {
+    private static let calendar = Calendar(identifier: .gregorian)
+    
+    private static var maxCacheAgeInDays: Int {
         return 7
     }
     
-    func validate(_ timestamp: Date, against date: Date) -> Bool {
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else {
             return false
         }
@@ -26,7 +28,6 @@ class LocalNewsLoader {
     
     private let store: NewsStore
     private let currentDate: () -> Date
-    private let cachePolicy =  NewsCachePolicy()
     
     init(store: NewsStore, currentDate: @escaping () -> Date) {
         self.store = store
@@ -70,7 +71,7 @@ extension LocalNewsLoader: NewsLoader {
             switch result {
             case let .failure(error):
                 completion(.failure(error))
-            case let .found(news, timestamp) where self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(news, timestamp) where NewsCachePolicy.validate(timestamp, against: self.currentDate()):
                 completion(.success(news.toModels()))
             case .found, .empty:
                 completion(.success([]))
@@ -87,7 +88,7 @@ extension LocalNewsLoader {
             switch result {
             case .failure:
                 self.store.deleteCachedNews { _ in }
-            case let .found(_, timestamp) where !self.cachePolicy.validate(timestamp, against: self.currentDate()):
+            case let .found(_, timestamp) where !NewsCachePolicy.validate(timestamp, against: self.currentDate()):
                 self.store.deleteCachedNews { _ in }
             case .empty, .found: break
             }
