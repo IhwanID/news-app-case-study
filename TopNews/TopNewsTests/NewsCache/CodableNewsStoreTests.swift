38081dbd5e104c82b193cfb3ec433758633f2454
Ihwan <ihwan.id@icloud.com>
2022-04-03 22:10:13 +0700

    xtract duplicate no-side-effects-on-retrieve test code into a reusable helper method

--- TopNews/TopNewsTests/NewsCache/CodableNewsStoreTests.swift
+++ TopNews/TopNewsTests/NewsCache/CodableNewsStoreTests.swift
@@ -88,23 +88,7 @@ class CodableNewsStoreTests: XCTestCase {
     
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache retrieval")
-        
-        sut.retrieve { firstResult in
-            sut.retrieve { secondResult in
-                switch (firstResult, secondResult) {
-                case (.empty, .empty):
-                    break
-                    
-                default:
-                    XCTFail("Expected retrieving twice from empty cache to deliver same empty result, got \(firstResult) and \(secondResult) instead")
-                }
-                
-                exp.fulfill()
-            }
-        }
-        
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toRetrieve: .empty)
     }
     
     func test_retrieveAfterInsertingToEmptyCache_deliversInsertedValues() {
@@ -130,29 +114,23 @@ class CodableNewsStoreTests: XCTestCase {
         
         sut.insert(news, timestamp: timestamp) { insertionError in
             XCTAssertNil(insertionError, "Expected news to be inserted successfully")
-            
-            sut.retrieve { firstResult in
-                sut.retrieve { secondResult in
-                    switch (firstResult, secondResult) {
-                    case let (.found(expectedNews, expectedTimestamp), .found(retrievedNews, retrievedTimestamp)):
-                        XCTAssertEqual(retrievedNews, expectedNews)
-                        XCTAssertEqual(retrievedTimestamp, expectedTimestamp)
-                    default:
-                        XCTFail("Expected retrieving twice from non empty cache to deliver same found result with news \(news) and timestamp \(timestamp), got \(firstResult) and \(secondResult) instead")
-                    }
-                    
-                    exp.fulfill()
-                }
-            }
+            exp.fulfill()
         }
         
         wait(for: [exp], timeout: 1.0)
+        
+        expect(sut, toRetrieveTwice: .found(news: news, timestamp: timestamp))
     }
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableNewsStore {
         let sut = CodableNewsStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func expect(_ sut: CodableNewsStore, toRetrieveTwice expectedResult: RetrieveCachedNewsResult, file: StaticString = #file, line: UInt = #line) {
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
+        expect(sut, toRetrieve: expectedResult, file: file, line: line)
     }
     
     private func expect(_ sut: CodableNewsStore, toRetrieve expectedResult: RetrieveCachedNewsResult, file: StaticString = #file, line: UInt = #line) {
