6a1dcb7e82df1784b8d551dc007d37140e8c6e67
ihwan <ihwan.id@icloud.com>
2022-04-04 11:16:19 +0700

    Group news store specs and helper

---
+++ TopNews/TopNewsTests/NewsCache/FeedStoreSpecs/XCTestCase+FailableDeleteNewsStoreSpecs.swift
@@ -0,0 +1,23 @@
+//
+//  XCTestCase+FailableDeleteNewsStoreSpecs.swift
+//  TopNewsTests
+//
+//  Created by Ihwan on 04/04/22.
+//
+
+import XCTest
+import TopNews
+
+extension FailableDeleteNewsStoreSpecs where Self: XCTestCase {
+    func assertThatDeleteDeliversErrorOnDeletionError(on sut: NewsStore, file: StaticString = #file, line: UInt = #line) {
+        let deletionError = deleteCache(from: sut)
+
+        XCTAssertNotNil(deletionError, "Expected cache deletion to fail", file: file, line: line)
+    }
+
+    func assertThatDeleteHasNoSideEffectsOnDeletionError(on sut: NewsStore, file: StaticString = #file, line: UInt = #line) {
+        deleteCache(from: sut)
+
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
