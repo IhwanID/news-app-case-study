72348793e84b740bbd2d2e1b2a5d896e867ab722
ihwan <ihwan.id@icloud.com>
2022-04-04 10:42:14 +0700

    Extract FailableInsertNewsStoreSpecs checks into reusable assertions

--- TopNews/TopNewsTests/NewsCache/CodableNewsStoreTests.swift
+++ TopNews/TopNewsTests/NewsCache/CodableNewsStoreTests.swift
@@ -80,23 +80,13 @@ class CodableNewsStoreTests: XCTestCase, FailableNewsStoreSpecs {
     func test_insert_deliversErrorOnInsertionError() {
         let invalidStoreURL = URL(string: "invalid://store-url")!
         let sut = makeSUT(storeURL: invalidStoreURL)
-        let news = uniqueNews().local
-        let timestamp = Date()
-        
-        let insertionError = insert((news, timestamp), to: sut)
-        
-        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+        assertThatInsertDeliversErrorOnInsertionError(on: sut)
     }
     
     func test_insert_hasNoSideEffectsOnInsertionError() {
         let invalidStoreURL = URL(string: "invalid://store-url")!
         let sut = makeSUT(storeURL: invalidStoreURL)
-        let news = uniqueNews().local
-        let timestamp = Date()
-        
-        insert((news, timestamp), to: sut)
-        
-        expect(sut, toRetrieve: .empty)
+        assertThatInsertHasNoSideEffectsOnInsertionError(on: sut)
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
---
+++ TopNews/TopNewsTests/NewsCache/XCTestCase+FailableInsertNewsStoreSpecs.swift
@@ -0,0 +1,23 @@
+//
+//  XCTestCase+FailableInsertNewsStoreSpecs.swift
+//  TopNewsTests
+//
+//  Created by Ihwan on 04/04/22.
+//
+
+import XCTest
+import TopNews
+
+extension FailableInsertNewsStoreSpecs where Self: XCTestCase {
+    func assertThatInsertDeliversErrorOnInsertionError(on sut: NewsStore, file: StaticString = #file, line: UInt = #line) {
+        let insertionError = insert((uniqueNews().local, Date()), to: sut)
+
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error", file: file, line: line)
+    }
+
+    func assertThatInsertHasNoSideEffectsOnInsertionError(on sut: NewsStore, file: StaticString = #file, line: UInt = #line) {
+        insert((uniqueNews().local, Date()), to: sut)
+
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
