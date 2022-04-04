58fa86d30f5ae4cc426d22a9dabcb592d816abf0
Ihwan <ihwan.id@icloud.com>
2022-04-05 04:53:20 +0700

    add empty CoreData news store specs

---
+++ TopNews/TopNewsTests/NewsCache/CoreDataNewsStoreTest.swift
@@ -0,0 +1,61 @@
+//
+//  CoreDataNewsStoreTest.swift
+//  TopNewsTests
+//
+//  Created by Ihwan on 05/04/22.
+//
+
+import XCTest
+
+class CoreDataNewsStoreTest: XCTestCase, NewsStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+    
+
+}
