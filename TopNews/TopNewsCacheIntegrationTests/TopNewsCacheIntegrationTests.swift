2164976f21c51063c90499963957cf87c1ac95ee
ihwan <ihwan.id@icloud.com>
2022-04-06 14:25:22 +0700

    Clean up & undo all cache side-effects on setUp and tearDown to avoid sharing state between tests

--- TopNews/TopNewsCacheIntegrationTests/TopNewsCacheIntegrationTests.swift
+++ TopNews/TopNewsCacheIntegrationTests/TopNewsCacheIntegrationTests.swift
@@ -9,42 +9,65 @@ import XCTest
 import TopNews
 
 class TopNewsCacheIntegrationTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+        undoStoreSideEffects()
+    }
     
     func test_load_deliversNoItemsOnEmptyCache() {
-            let sut = makeSUT()
-
-            let exp = expectation(description: "Wait for load completion")
-            sut.load { result in
-                switch result {
-                case let .success(news):
-                    XCTAssertEqual(news, [], "Expected empty news")
-
-                case let .failure(error):
-                    XCTFail("Expected successful news result, got \(error) instead")
-                }
-
-                exp.fulfill()
-            }
-            wait(for: [exp], timeout: 1.0)
-        }
-
-        // MARK: Helpers
+        let sut = makeSUT()
         
-        private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalNewsLoader {
-            let storeBundle = Bundle(for: CoreDataNewsStore.self)
-            let storeURL = testSpecificStoreURL()
-            let store = try! CoreDataNewsStore(storeURL: storeURL, bundle: storeBundle)
-            let sut = LocalNewsLoader(store: store, currentDate: Date.init)
-            trackForMemoryLeaks(store, file: file, line: line)
-            trackForMemoryLeaks(sut, file: file, line: line)
-            return sut
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(news):
+                XCTAssertEqual(news, [], "Expected empty news")
+                
+            case let .failure(error):
+                XCTFail("Expected successful news result, got \(error) instead")
+            }
+            
+            exp.fulfill()
         }
-
-        private func testSpecificStoreURL() -> URL {
-            return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
-        }
-
-        private func cachesDirectory() -> URL {
-            return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
-        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+    // MARK: Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalNewsLoader {
+        let storeBundle = Bundle(for: CoreDataNewsStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataNewsStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalNewsLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtifacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtifacts()
+    }
+    
+    private func deleteStoreArtifacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appendingPathComponent("\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
