84e840bfdd6c7096ca7ae1971d7fcfc5392af595
Ihwan <ihwan.id@icloud.com>
2022-04-14 05:41:32 +0700

    Replace NewsViewControllerTests.LoaderSpy references in the production types with the production `NewsLoader` protocol abstraction

--- TopNews/TopNewsiOSTests/NewsViewControllerTests.swift
+++ TopNews/TopNewsiOSTests/NewsViewControllerTests.swift
@@ -6,12 +6,13 @@
 //
 
 import XCTest
+import TopNews
 
 final class NewsViewController: UIViewController {
     
-    private var loader: NewsViewControllerTests.LoaderSpy?
+    private var loader: NewsLoader?
     
-    convenience init(loader: NewsViewControllerTests.LoaderSpy) {
+    convenience init(loader: NewsLoader) {
         self.init()
         self.loader = loader
     }
@@ -19,7 +20,7 @@ final class NewsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        loader?.load()
+        loader?.load { _ in }
     }
 }
 
@@ -43,10 +44,10 @@ class NewsViewControllerTests: XCTestCase {
     
     // MARK: - Helpers
     
-    class LoaderSpy {
+    class LoaderSpy: NewsLoader {
         private(set) var loadCallCount: Int = 0
         
-        func load() {
+        func load(completion: @escaping (NewsLoader.Result) -> Void) {
             loadCallCount += 1
         }
     }
