9e92b6c593da25d2473c271d2cfcb95a351c8ef3
Ihwan <ihwan.id@icloud.com>
2022-04-13 23:24:44 +0700

    render prototype data on the top of tableview

--- Prototype/Prototype/NewsItemCell.swift
+++ Prototype/Prototype/NewsItemCell.swift
@@ -12,5 +12,4 @@ class NewsItemCell: UITableViewCell {
     @IBOutlet weak var sourceLabel: UILabel!
     @IBOutlet weak var newsImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
-    
 }
--- Prototype/Prototype/NewsViewController.swift
+++ Prototype/Prototype/NewsViewController.swift
@@ -18,7 +18,7 @@ extension NewsItemViewModel {
     static var prototypeNews: [NewsItemViewModel] {
             return [
                 NewsItemViewModel(author: "Budi", source: "CNN", imageName: "image-1", title: "Contoh Berita 1"),
-                NewsItemViewModel(author: "Budi", source: "CNN", imageName: "image-1", title: "Contoh Berita 1"),
+                NewsItemViewModel(author: nil, source: "CNN", imageName: "image-1", title: "Contoh Berita 1"),
                 NewsItemViewModel(author: "Budi", source: "CNN", imageName: "image-1", title: "Contoh Berita 1"),
                 NewsItemViewModel(author: "Budi", source: "CNN", imageName: "image-1", title: "Contoh Berita 1"),
                 NewsItemViewModel(author: "Budi", source: "CNN", imageName: "image-1", title: "Contoh Berita 1"),
@@ -29,17 +29,34 @@ extension NewsItemViewModel {
 
 final class NewsViewController: UITableViewController {
     
+    private let news = NewsItemViewModel.prototypeNews
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return tableView.dequeueReusableCell(withIdentifier: "NewsItemCell")!
+        let cell =  tableView.dequeueReusableCell(withIdentifier: "NewsItemCell", for: indexPath) as! NewsItemCell
+        let model = news[indexPath.row]
+        cell.configure(with: model)
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return news.count
     }
     
+}
+
+extension NewsItemCell {
+    func configure(with model: NewsItemViewModel) {
+        authorLabel.text = model.author
+        authorLabel.isHidden = model.author == nil
+
+        sourceLabel.text = model.source
+        titleLabel.text = model.title
+
+        newsImageView.image = UIImage(named: model.imageName)
+    }
 }
