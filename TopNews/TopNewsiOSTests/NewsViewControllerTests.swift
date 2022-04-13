61a1d09147a329dfa803dcc2128c8c7dede53740
Ihwan <ihwan.id@icloud.com>
2022-04-14 06:18:17 +0700

    Move NewsViewController to production target

--- TopNews/TopNewsiOSTests/NewsViewControllerTests.swift
+++ TopNews/TopNewsiOSTests/NewsViewControllerTests.swift
@@ -7,31 +7,7 @@
 
 import XCTest
 import TopNews
-
-final class NewsViewController: UITableViewController {
-    
-    private var loader: NewsLoader?
-    
-    convenience init(loader: NewsLoader) {
-        self.init()
-        self.loader = loader
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        refreshControl = UIRefreshControl()
-        refreshControl?.addTarget(self, action: #selector(load), for: .valueChanged)
-        load()
-        
-    }
-    
-    @objc private func load() {
-        refreshControl?.beginRefreshing()
-        loader?.load { [weak self] _ in
-            self?.refreshControl?.endRefreshing()
-        }
-    }
-}
+import TopNewsiOS
 
 class NewsViewControllerTests: XCTestCase {
     
