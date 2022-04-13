d62fdcc81f3692b731f0940d749e80224c90548a
Ihwan <ihwan.id@icloud.com>
2022-04-13 22:26:01 +0700

    add empty prototype project

---
+++ Prototype/Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Ihwan on 13/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
