b734ef6776fc865377398278c44468483c462f4c
Ihwan <ihwan.id@icloud.com>
2022-04-14 05:38:26 +0700

    load news on view did load

--- TopNews/TopNewsiOSTests/NewsViewControllerTests.swift
+++ TopNews/TopNewsiOSTests/NewsViewControllerTests.swift
@@ -7,9 +7,19 @@
 
 import XCTest
 
-final class NewsViewController {
-    init(loader: NewsViewControllerTests.LoaderSpy) {
+final class NewsViewController: UIViewController {
+    
+    private var loader: NewsViewControllerTests.LoaderSpy?
+    
+    convenience init(loader: NewsViewControllerTests.LoaderSpy) {
+        self.init()
+        self.loader = loader
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
+        loader?.load()
     }
 }
 
@@ -22,10 +32,23 @@ class NewsViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadCallCount, 0)
     }
     
+    func test_viewDidLoad_loadsNews() {
+        let loader = LoaderSpy()
+        let sut = NewsViewController(loader: loader)
+        
+        sut.loadViewIfNeeded()
+        
+        XCTAssertEqual(loader.loadCallCount, 1)
+    }
+    
     // MARK: - Helpers
     
     class LoaderSpy {
         private(set) var loadCallCount: Int = 0
+        
+        func load() {
+            loadCallCount += 1
+        }
     }
     
 }
