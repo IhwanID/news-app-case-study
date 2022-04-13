7a0ba4bca47a4126a637d8082e1eceecbab064fe
Ihwan <ihwan.id@icloud.com>
2022-04-13 23:18:36 +0700

    add NewsItemCell with storyboard connections

---
+++ Prototype/Prototype/NewsItemCell.swift
@@ -0,0 +1,16 @@
+//
+//  NewsItemCell.swift
+//  Prototype
+//
+//  Created by Ihwan on 13/04/22.
+//
+
+import UIKit
+
+class NewsItemCell: UITableViewCell {
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var sourceLabel: UILabel!
+    @IBOutlet weak var newsImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+}
