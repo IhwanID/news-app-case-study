4d43391fe46c87dede3407a43b592927ad0ff1c0
Ihwan <ihwan.id@icloud.com>
2022-04-16 17:01:37 +0700

    Remove mutable state from NewsViewModel. The NewsViewModel only needs to forward state changes, so state is only transient.

--- TopNews/TopNewsiOS/NewsUI/Model/NewsViewModel.swift
+++ TopNews/TopNewsiOS/NewsUI/Model/NewsViewModel.swift
@@ -9,26 +9,24 @@ import Foundation
 import TopNews
 
 final class NewsViewModel {
+    typealias Observer<T> = (T) -> Void
+    
     private let newsLoader: NewsLoader
 
     init(newsLoader: NewsLoader) {
         self.newsLoader = newsLoader
     }
 
-    var onChange: ((NewsViewModel) -> Void)?
-    var onNewsLoad: (([NewsItem]) -> Void)?
-
-    private(set) var isLoading: Bool = false {
-        didSet { onChange?(self) }
-    }
+    var onLoadingStateChange: Observer<Bool>?
+    var onNewsLoad: Observer<[NewsItem]>?
 
     func loadNews() {
-        isLoading = true
+        onLoadingStateChange?(true)
         newsLoader.load { [weak self] result in
             if let news = try? result.get() {
                 self?.onNewsLoad?(news)
             }
-            self?.isLoading = false
+            self?.onLoadingStateChange?(false)
         }
     }
 }
