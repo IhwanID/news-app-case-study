222d6186d4deabcd692c825a6aea8b8906645890
Ihwan <ihwan.id@icloud.com>
2022-04-19 00:32:42 +0700

    Make MainQueueDispatchDecorator generic

--- TopNews/TopNewsiOS/NewsUI/Composers/NewsUIComposer.swift
+++ TopNews/TopNewsiOS/NewsUI/Composers/NewsUIComposer.swift
@@ -21,22 +21,26 @@ public final class NewsUIComposer {
     }
 }
 
-private final class MainQueueDispatchDecorator: NewsLoader {
-    private let decoratee: NewsLoader
-
-    init(decoratee: NewsLoader) {
+private final class MainQueueDispatchDecorator<T> {
+    private let decoratee: T
+    
+    init(decoratee: T) {
         self.decoratee = decoratee
     }
+    
+    func dispatch(completion: @escaping () -> Void) {
+        guard Thread.isMainThread else {
+            return DispatchQueue.main.async(execute: completion)
+        }
+        
+        completion()
+    }
+}
 
+extension MainQueueDispatchDecorator: NewsLoader where T == NewsLoader {
     func load(completion: @escaping (NewsLoader.Result) -> Void) {
-        decoratee.load { result in
-            if Thread.isMainThread {
-                completion(result)
-            } else {
-                DispatchQueue.main.async {
-                    completion(result)
-                }
-            }
+        decoratee.load { [weak self] result in
+            self?.dispatch { completion(result) }
         }
     }
 }
