7e152df9d168f947b1d1df627b3bc1bcc349afd8
ihwan <ihwan.id@icloud.com>
2022-04-20 21:03:35 +0700

    Inline Data creation

--- TopNews/TopNewsTests/NewsPresentation/NewsImagePresenterTest.swift
+++ TopNews/TopNewsTests/NewsPresentation/NewsImagePresenterTest.swift
@@ -80,9 +80,8 @@ class NewsImagePresenterTests: XCTestCase {
     func test_didFinishLoadingImageData_displaysRetryOnFailedImageTransformation() {
         let (sut, view) = makeSUT(imageTransformer: fail )
         let news = uniqueItem()
-        let data = Data()
         
-        sut.didFinishLoadingImageData(with: data, for: news)
+        sut.didFinishLoadingImageData(with: Data(), for: news)
         
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
@@ -95,11 +94,10 @@ class NewsImagePresenterTests: XCTestCase {
     
     func test_didFinishLoadingImageData_displaysImageOnSuccessfulTransformation() {
         let image = uniqueItem()
-        let data = Data()
         let transformedData = AnyImage()
         let (sut, view) = makeSUT(imageTransformer: { _ in transformedData })
         
-        sut.didFinishLoadingImageData(with: data, for: image)
+        sut.didFinishLoadingImageData(with:  Data(), for: image)
         
         let message = view.messages.first
         XCTAssertEqual(view.messages.count, 1)
