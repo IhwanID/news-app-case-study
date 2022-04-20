9835411762b8d66d202b9af1428dfbd1250e4d8d
ihwan <ihwan.id@icloud.com>
2022-04-20 20:45:42 +0700

    NewsImagePresenter displays loading image on `didStartLoadingImageData`

--- TopNews/TopNewsTests/NewsPresentation/NewsImagePresenterTest.swift
+++ TopNews/TopNewsTests/NewsPresentation/NewsImagePresenterTest.swift
@@ -6,21 +6,59 @@
 //
 
 import XCTest
+import TopNews
+
+struct NewsImageViewModel {
+    let author: String?
+    let title: String?
+    let image: Any?
+    let isLoading: Bool
+    let shouldRetry: Bool
+    
+    var hasAuthor: Bool {
+        return author != nil
+    }
+}
+
+protocol NewsImageView {
+    func display(_ model: NewsImageViewModel)
+}
 
 class NewsImagePresenter {
-    init(view: Any) {
-
+    private let view: NewsImageView
+    
+    init(view: NewsImageView) {
+        self.view = view
+    }
+    
+    func didStartLoadingImageData(for model: NewsItem) {
+        view.display(NewsImageViewModel(author: model.author, title: model.title, image: nil, isLoading: true, shouldRetry: false))
     }
 }
 
 class NewsImagePresenterTests: XCTestCase {
-
+    
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
-
+        
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
-
+    
+    func test_didStartLoadingImageData_displaysLoadingImage() {
+        let (sut, view) = makeSUT()
+        let news = uniqueItem()
+        
+        sut.didStartLoadingImageData(for: news)
+        
+        let message = view.messages.first
+        XCTAssertEqual(view.messages.count, 1)
+        XCTAssertEqual(message?.author, news.author)
+        XCTAssertEqual(message?.title, news.title)
+        XCTAssertEqual(message?.isLoading, true)
+        XCTAssertEqual(message?.shouldRetry, false)
+        XCTAssertNil(message?.image)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: NewsImagePresenter, view: ViewSpy) {
@@ -30,9 +68,14 @@ class NewsImagePresenterTests: XCTestCase {
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, view)
     }
-
-    private class ViewSpy {
-        let messages = [Any]()
+    
+    private class ViewSpy: NewsImageView {
+        
+        private(set) var messages = [NewsImageViewModel]()
+        
+        func display(_ model: NewsImageViewModel) {
+            messages.append(model)
+        }
     }
-
+    
 }
